824379038577c279e066bb084e5ea1375b3a4adb
Emanuel Martinez <yinproducciones@gmail.com>
2024-01-02 17:26:26 -0300

    add:feat product detail section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section className="bg-pink-200 h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
+    <section className="h-[800px] bg-hero bg-no-repeat bg-cover bg-center py-24">
       <div className="container mx-auto flex justify-around h-full">
         {/* text */}
         <div className="flex flex-col justify-center">
-          {/* pretitle */}
+          {/* pre title */}
           <div className="font-semibold flex items-center uppercase">
             <div className="w-10 h-[2px] bg-red-500 mr-3"></div>New Trend
           </div>
